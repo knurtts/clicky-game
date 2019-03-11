8dc027bcd47bea1df715eec0c3e4667252ef06e0
Robert <robertwinstonanderson@gmail.com>
2019-03-10 22:50:45 -0600

    img bug fixes

--- clicky-game/src/components/GojiCard/GojiCard.js
+++ clicky-game/src/components/GojiCard/GojiCard.js
@@ -6,7 +6,7 @@ export default function GojiCard(props) {
     return (
         <div className="card shake">
         <img 
-            src={require(props.src)} className="card-img-top" 
+            src={props.src} className="card-img-top" 
             alt={props.name} 
             onClick={() => {props.clicked(props.idCard)}} 
             id={props.idCard}
